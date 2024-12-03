ddf92af1f9ec53ca4804e887d3dbc7067612e644
olajimmy <overdedicated9@gmail.com>
2024-12-03 09:49:28 -0500

    we can now get values by ids

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -4,6 +4,29 @@ import db from "../db/conn.mjs";
 
 const router = express.Router();
 
+//FULL CRUD FUNCTIONALITY
+//C- CREATE -POST
+//R - READ - GET
+//U - UPDATE - PUT/PATCH
+//D - DELETE - DELETE
+
+//for read, we usually do an index route and a show route
+//Index displays or gets many db items
+
+//------------------------------------------------
+//                      All ROutes
+//------------------------------------------------
+
+//   Get implements READ functionality---------------------------
+//we want to be careful with this get route
+//because it could be a huge amount of data
+//      that is why we limit to 50 in this example
+//          if you wanted to either use pagination or
+//          somehow iterate through, you might combine limit(n) and skip(m)
+// ===== make sure that you are using async-await
+// because db access requests are asynchronous, but we need that data
+// before we move on
+
 router.get("/", async (req, res) => {
   let collection = await db.collection("grades");
   let results = await collection.find({}).limit(50).toArray();
@@ -11,4 +34,22 @@ router.get("/", async (req, res) => {
   else res.send(results).status(200);
 });
 
+//the show route is READ, but limiting to a specific entry
+//in this case, we'll use id to get a specific grades entry
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+
+  //define the query
+  //in this query, we are searching for a specific id
+  let query;
+  try {
+    query = { _id: new ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("not found").status(404);
+    else res.send(result).status(200);
+  } catch (err) {
+    res.send("not an id").status(400);
+  }
+});
 export default router;
